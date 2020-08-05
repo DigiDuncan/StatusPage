80161f4454b96e2687e7dc5f201a8a7d391b6004
Natalie Fearnley <nfearnley@gmail.com>
2020-08-04 21:16:58 -0400

    Added update page

--- status.py
+++ status.py
@@ -1,17 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import arrow
 
 app = Flask(__name__)
 
-s = "dead"
-d = "(not really)"
-dis = "online"
+s = "LOADING"
+d = ""
 lu = arrow.now()
-
-if not s.endswith((".", "!", "?")):
-    s += "."
+dis = "online"
 
 
 @app.route("/")
 def home():
     return render_template("status.html", status=s, details=d, discord=dis, lastupdated=lu)
+
+
+@app.route("/update")
+def update_page():
+    return render_template("update.html")
+
+
+@app.route("/update", methods=["POST"])
+def update():
+    global s
+    global d
+    global lu
+    status = request.form.get("status")
+    details = request.form.get("details")
+    if not status.endswith((".", "!", "?")):
+        status += "."
+    s = status
+    d = details
+    lu = arrow.now()
+    return redirect(url_for("home"))
